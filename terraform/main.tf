2e8c3bc3f5b265db9e6878c42925b34886be57b0
Zabih Keraam <keraamzabih@gmail.com>
2025-04-12 15:30:24 +0430

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+
+
+# _____________________Creating App-runner___________________
+resource "aws_apprunner_service" "backend_service" {
+  service_name = "${var.project}-backend-${random_id.bucket_suffix.hex}"
+
+  source_configuration {
+    authentication_configuration {
+      connection_arn = "arn:aws:apprunner:us-east-1:135808921133:connection/new-connection/45c0ab0285b64f8abd68e04dde58f1ff"
+    }
+
+    auto_deployments_enabled = true
+
+    code_repository {
+      repository_url = var.repository_url
+      source_code_version {
+        type  = "BRANCH"
+        value = var.branch
+      }
+
+      code_configuration {
+        configuration_source = "API"
+        code_configuration_values {
+        #   runtime        = "NODEJS_18"
+          runtime        = "PYTHON_3.11"
+        #   build_command = "npm --prefix ./backend install --production"
+          build_command = "pip install -r ./backend/requirements.txt --target ."
+        #   start_command = "node ./backend/server.js" 
+          start_command = "uvicorn ./backend.main:app --host 0.0.0.0 --port 8080 --reload" 
+          port           = 8080
+          runtime_environment_variables = {
+            NODE_ENV        = "production"
+          }
+        }
+      }
+    }
+  }
+
+  instance_configuration {
+    cpu               = "1024"
+    memory            = "2048"
+  }
+
+    tags = {
+    Name        = "${var.project}--backend"
+    Project     = var.project
+    Environment = "production"
+  }
+
+  health_check_configuration {
+    protocol            = "HTTP"
+    path                = "/health"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+  }
+
+  depends_on = [
+    aws_cloudfront_distribution.cdn,
+    aws_s3_bucket.frontend_bucket,
+    aws_db_instance.my_database
+  ]
+}
+
+
+output "apprunner_service_url" {
+  value       = aws_apprunner_service.backend_service.service_url
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,8 @@
+region = "us-east-1"
+customer_name = "Zabihullah"
+project       = "portfolio"
+repository_url = "https://github.com/Zabihkeraam1/project-template.git"
+branch = "master"
+db_password = "your_secure_password"
+db_username = "postgres"
+db_name = "myDatabase"
