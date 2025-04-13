3e6faaf2400a6458337a19835026ea6b404abcf9
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-13 21:41:11 +0430

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,8 +36,24 @@ resource "aws_apprunner_service" "backend_service" {
       }
 
       code_configuration {
-        configuration_source = "REPOSITORY"
-        # When using 'REPOSITORY', App Runner reads the configuration from 'apprunner.yaml' in your repository.
+        configuration_source = "API"
+         code_configuration_values {
+          runtime        = "PYTHON_311"
+          build_command = "python3 -m venv /app/venv && source /app/venv/bin/activate && pip3 install --upgrade pip && pip3 install fastapi uvicorn && pip3 install -r ./backend/requirements.txt && pip3 list"
+          start_command = "pip3 install --upgrade pip && source /app/venv/bin/activate && pip3 install fastapi uvicorn && pip3 install -r ./backend/requirements.txt && uvicorn backend.main:app --host 0.0.0.0 --port 8080" 
+          port           = 8080
+          runtime_environment_variables = {
+            NODE_ENV        = "production"
+            FRONTEND_DOMAIN  = "aws_cloudfront_distribution.cdn.domain_name"
+            S3_BUCKET_NAME  = "aws_s3_bucket.frontend_bucket.bucket"
+            DB_USER           = "aws_db_instance.my_database.username"
+            DB_PASSWORD       = "var.db_password"
+            DB_HOST           = "aws_db_instance.my_database.address"
+            DB_NAME           = "aws_db_instance.my_database.db_name"
+            DB_PORT           = "5432"
+            DB_SSL            = "true" # Always use SSL with public RDS
+          }
+        }
       }
     }
   }
