56105f175b98badef63e76ca475bcc8e2026772a
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-14 11:52:34 +0430

    Update build.sh

--- terraform/build.sh
+++ terraform/build.sh
@@ -8,12 +8,12 @@ python3 -m venv /app/venv
 source /app/venv/bin/activate
 
 # Upgrade pip and install dependencies
-pip install --upgrade pip
-pip install fastapi uvicorn
-pip install -r ./backend/requirements.txt
+pip3 install --upgrade pip
+pip3 install fastapi uvicorn
+pip3 install -r ./backend/requirements.txt
 
 # Ensure start.sh is executable
 # chmod +x ./start.sh
 
 # List installed packages
-pip list
+pip3 list
